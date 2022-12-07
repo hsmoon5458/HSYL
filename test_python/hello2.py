cc6fabd1c2a26d2a374d302d103d0372a8e86826
Lim Yelim <yelim2377@gmail.com>
2022-12-06 23:43:21 -0800

    hello2.py file added

---
+++ test_python/hello2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+name, age, location = input("Enter your name, age and location with a space in between:").split()
+print("Hello, this is", name, ". I am", age, "years old, living in", location, ".")
